f967f3fb1336ce0975864e06f3707b78b0cb064a
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-04-28 09:33:18 -0400

    Add recipe button, improve styling slightly

--- RecipePal/Pages/Recipes/Create.cshtml.cs
+++ RecipePal/Pages/Recipes/Create.cshtml.cs
@@ -1,11 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipePal.Models;
+using RecipePal.Models.Repositories;
 
 namespace RecipePal.Pages.Recipes
 {
     public class CreateModel : PageModel
     {
+        readonly IRepository<Recipe> _recipesRepo;
+        readonly IRepository<Cookbook> _cookbooksRepo;
+
+        public Cookbook Cookbook { get; set; }
+        public Recipe Recipe { get; set; }
+        
+        public CreateModel(IRepository<Recipe> recipes, IRepository<Cookbook> cookbooks)
+        {
+            _recipesRepo = recipes;
+            _cookbooksRepo = cookbooks;
+        }
+
         public void OnGet(int id)
         {
+            Cookbook = _cookbooksRepo.Get(id);
+        }
+
+        public IActionResult OnPost(Recipe recipe)
+        {
+            Recipe = _recipesRepo.Insert(recipe);
+            return RedirectToPage("../Users/EditCookbook", new { id = Recipe.CookbookId });
         }
     }
 }
